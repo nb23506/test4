f900099927403f139c3319a4dfe7b1498cf011e1
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-09-30 15:49:35 +0900

    Update program.c

--- program.c
+++ program.c
@@ -7,26 +7,6 @@ int larger_number(int x, int y) {
     } else if (y > x) {
         return y;
     } else {
-        return -1; // Menandakan bahwa kedua angka samaa
+        return -1; // Menandakan bahwa kedua angka sama
     }
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <integer x> <integer y>\n", argv[0]);
-        return 1;
-    }
-
-    int x = atoi(argv[1]);
-    int y = atoi(argv[2]);
-    
-    int result = larger_number(x, y);
-    
-    if (result == -1) {
-        printf("Both numbers are equal: %d\n", x);
-    } else {
-        printf("The larger number is: %d\n", result);
-    }
-
-    return 0;
 }
