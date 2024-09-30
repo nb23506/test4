d37f4857ad2d1c14377e3ba21c5cdf548242a5db
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-09-30 16:14:25 +0900

    Update program.c

--- program.c
+++ program.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int larger_number(int x, int y) {
+int main() {
+    int x, y;
+
+    // Membaca input dari pengguna
+    printf("Masukkan dua angka bulat (x dan y): ");
+    scanf("%d %d", &x, &y);
+
+    // Menentukan dan mencetak angka yang lebih besar
     if (x > y) {
-        return x;
+        printf("Angka yang lebih besar adalah: %d\n", x);
     } else if (y > x) {
-        return y;
+        printf("Angka yang lebih besar adalah: %d\n", y);
     } else {
-        return -1; // Menandakan bahwa kedua angka sama
+        printf("Kedua angka sama.\n");
     }
+
+    return 0;
 }
+
