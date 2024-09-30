bfc1ddbab0ecc57e89046b421219ba25bb1db488
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-09-30 15:44:46 +0900

    Update program.c

--- program.c
+++ program.c
@@ -1,4 +1,32 @@
-// program.c
-int add(int a, int c) {
-    return a + c;
+#include <stdio.h>
+#include <stdlib.h>
+
+int larger_number(int x, int y) {
+    if (x > y) {
+        return x;
+    } else if (y > x) {
+        return y;
+    } else {
+        return -1; // Menandakan bahwa kedua angka sama
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <integer x> <integer y>\n", argv[0]);
+        return 1;
+    }
+
+    int x = atoi(argv[1]);
+    int y = atoi(argv[2]);
+    
+    int result = larger_number(x, y);
+    
+    if (result == -1) {
+        printf("Both numbers are equal: %d\n", x);
+    } else {
+        printf("The larger number is: %d\n", result);
+    }
+
+    return 0;
 }
